c8e520e173c137aa35029a6537499323f1ddad87
郭永平 <291325561@qq.com>
2020-02-26 21:21:18 +0800

    基础题库完成

--- hmmm-frontend/src/module-hmmm/pages/questions.vue
+++ hmmm-frontend/src/module-hmmm/pages/questions.vue
@@ -4,53 +4,89 @@
       <el-card class="box-card">
         <!-- 第一行 -->
         <el-row :gutter="20">
-          <el-col :span="6">学&nbsp;&nbsp;&nbsp;&nbsp;科：
+          <el-col :span="6">学&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 科：
               <el-select v-model="searchForm.subjectID" placeholder="请选择" class="md">
                 <el-option v-for="item in subjectIDList" :key="item.value" :label="item.label" :value="item.value"></el-option>
-              </el-select>  
+              </el-select>
           </el-col>
           <el-col :span="6">难&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 度：
              <el-select v-model="searchForm.difficulty" placeholder="请选择" class="md">
                 <el-option v-for="item in difficultyList" :key="item.value" :label="item.label" :value="item.value"></el-option>
-              </el-select>  
+              </el-select>
           </el-col>
           <el-col :span="6">试题类型：
              <el-select v-model="searchForm.questionType" placeholder="请选择" class="md">
                 <el-option v-for="item in questionTypeList" :key="item.value" :label="item.label" :value="item.value"></el-option>
-              </el-select>  
+              </el-select>
           </el-col>
-          <el-col :span="6">标签：
+          <el-col :span="6">标&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 签：
             <el-select v-model="searchForm.tags" placeholder="请选择" class="md">
                 <el-option v-for="item in tagNameList" :key="item.value" :label="item.label" :value="item.value"></el-option>
             </el-select>
           </el-col>
         </el-row>
           <!-- 第二行 -->
-        <el-row :gutter="20" style="margin-top:20px">
-          <el-col :span="6">录入人：
-              <el-select v-model="searchForm.user" placeholder="请选择" class="md">
+        <el-row :gutter="20">
+          <el-col :span="6">城&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 市：
+              <el-select @change="searchForm.city=''" v-model="searchForm.province" placeholder="选城市" class="md" style="width:83px">
+                <el-option v-for="item in provinces()" :key="item" :label="item" :value="item"></el-option>
+              </el-select>
+              <el-select  v-model="searchForm.city" placeholder="选区县" class="md" style="width:83px">
+                <el-option  v-for="item in citys(searchForm.province)" :key="item" :label="item" :value="item"></el-option>
+              </el-select>
+          </el-col>
+          <el-col :span="6">关&nbsp; 键 &nbsp;字：
+              <el-input v-model="searchForm.keyword" placeholder="请输入题目编号/题干" class="md"></el-input>
+          </el-col>
+          <el-col :span="6">题目备注：
+              <el-input v-model="searchForm.remarks" placeholder="请输入" class="md"></el-input>
+          </el-col>
+          <el-col :span="6">企业简称：
+              <el-input v-model="searchForm.shortName" placeholder="请输入" class="md"></el-input>
+          </el-col>
+        </el-row>
+        <!-- 第三行 -->
+        <el-row :gutter="20">
+          <el-col :span="6">方&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 向：
+              <el-select v-model="searchForm.direction" placeholder="请选择" class="md">
+                <el-option v-for="item in directionList" :key="item" :label="item" :value="item"></el-option>
+              </el-select>
+          </el-col>
+          <el-col :span="6">录&nbsp; 入&nbsp; 人：
+              <el-select v-model="searchForm.creatorID" placeholder="请选择" class="md">
                 <el-option v-for="item in creatorList" :key="item.value" :label="item.username" :value="item.id"></el-option>
               </el-select>
           </el-col>
           <el-col :span="6">二级目录：
-              <el-select v-model="searchForm.directorys" placeholder="请选择" class="md">
+              <el-select v-model="searchForm.catalogID" placeholder="请输入二级目录" class="md">
                 <el-option v-for="item in directorysList" :key="item.id" :label="item.label" :value="item.value"></el-option>
-              </el-select> 
+              </el-select>
           </el-col>
-          <el-col :span="6">方&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 向：
-              <el-select v-model="searchForm.directions" placeholder="请输入题目编号/题干" class="md">
-                <el-option v-for="item in directionList" :key="item" :label="item" :value="item"></el-option>
-              </el-select> 
-          </el-col>
-          <el-col :span="6">城市：
-              <el-select @change="getCitye(searchForm.provincess)" v-model="searchForm.provincess" placeholder="选城市" class="md" style="width:83px">
-                <el-option v-for="item in provinces()" :key="item" :label="item" :value="item"></el-option>
-              </el-select> 
-              <el-select  v-model="searchForm.city" placeholder="选区县" class="md" style="width:83px">
-                <el-option  v-for="item in cityslist" :key="item" :label="item" :value="item"></el-option>
-              </el-select> 
+          <el-col :span="6">
+              <el-button size="mini">成功按钮</el-button>
+              <el-button type="primary" size="mini">主要按钮</el-button>
           </el-col>
         </el-row>
+      <el-table :data="questionsList" style="width: 100%" >
+      <el-table-column type="index" label="序号" width="60"></el-table-column>
+      <el-table-column prop="number" label="试题编号" ></el-table-column>
+      <el-table-column prop="subject" label="学科" ></el-table-column>
+      <el-table-column prop="questionType" label="题型" :formatter="questionTypeFMT"></el-table-column>
+      <el-table-column prop="question" label="题干" ></el-table-column>
+      <el-table-column prop="addDate" label="录入时间" >
+        <template slot-scope="stData">
+          {{stData.row.addDate|parseTimeByString}}
+        </template>
+      </el-table-column>
+      <el-table-column prop="difficulty" label="难度" :formatter="difficultyListFMT"></el-table-column>
+      <el-table-column prop="creator" label="录入人" ></el-table-column>
+      <el-table-column prop="date" label="操作" width="200">
+        <a href="#">预览</a>
+        <a href="#">修改</a>
+        <a href="#">删除</a>
+        <a href="#">加入精选</a>
+      </el-table-column>
+      </el-table>
       </el-card>
     </div>
   </div>
@@ -59,9 +95,10 @@
 <script>
 import {simple} from '@/api/hmmm/subjects.js'
 import {simple as simpleList} from '@/api/hmmm/tags.js'
+import {list} from '@/api/hmmm/questions.js'
 import {simple as usersSimple} from '@/api/base/users.js'
 import {simple as directorysSimple} from '@/api/hmmm/directorys.js'
-import { provinces, citys } from '@/api/hmmm/citys.js'
+import {provinces, citys} from '@/api/hmmm/citys.js'
 import {
   difficulty as difficultyList,
   questionType as questionTypeList,
@@ -69,9 +106,16 @@ import {
   } from '@/api/hmmm/constants.js'
 export default {
   name: 'QuestionsList',
+  watch: {
+    searchForm: {
+      handler: function(newV) {
+        this.getquestionsList()
+      },
+      deep: true
+    }
+  },
   data() {
     return {
-      provinces,
       directionList,
       questionTypeList,
       difficultyList,
@@ -80,16 +124,20 @@ export default {
       creatorList: [],
       subjectIDList: [],
       directorysList: [],
+      questionsList: [],
       searchForm: {
         subjectID: '',
         difficulty: '',
         questionType: '',
         tags: '',
-        user: '',
-        directorys: '',
-        directions: '',
-        provincess: '',
-        city: ''
+        creatorID: '',
+        catalogID: '',
+        direction: '',
+        province: '',
+        city: '',
+        keyword: '',
+        remarks: '',
+        shortName: ''
       }
     }
   },
@@ -98,8 +146,17 @@ export default {
     this.gettagNameList()
     this.getcreatorList()
     this.getdirectorysList()
+    this.getquestionsList()
   },
   methods: {
+    provinces,
+    citys,
+    difficultyListFMT (row, column, cellValue, index) {
+      return this.difficultyList[cellValue - 1].label
+    },
+    questionTypeFMT (row, column, cellValue, index) {
+     return this.questionTypeList[cellValue - 1].label
+    },
     async getsubjectIDList () {
       const result = await simple()
       this.subjectIDList = result.data
@@ -109,16 +166,16 @@ export default {
       this.tagNameList = result.data
     },
     async getcreatorList () {
-      const result = await usersSimple()   
+      const result = await usersSimple()
       this.creatorList = result.data
     },
     async getdirectorysList () {
       const result = await directorysSimple()
       this.directorysList = result.data
     },
-    getCitye (pname) {
-      this.searchForm.city = ''
-      this.cityslist = citys(pname)
+    async getquestionsList () {
+      const result = await list(this.searchForm)
+      this.questionsList = result.data.items
     }
   }
 }
@@ -127,5 +184,11 @@ export default {
 <style scoped>
 .md {
   width: 170px;
+}
+.el-col {
+  margin-bottom: 10px;
+}
+.el-table {
+  margin-top: 20px;
 }
 </style>
